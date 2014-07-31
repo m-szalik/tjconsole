202bfee1867b716cbef130966e2ae9764559b114
m-szalik <szalik@jsoftwae.org>
2014-07-31 21:15:22 +0100

    Java tools.jar to discover local processes.

--- src/main/java/com/gruszecm/tjconsole/ProcessListManager.java
+++ src/main/java/com/gruszecm/tjconsole/ProcessListManager.java
@@ -1,7 +1,12 @@
 package com.gruszecm.tjconsole;
 
+import sun.management.ConnectorAddressLink;
+
 import javax.management.remote.JMXServiceURL;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 
@@ -9,7 +14,6 @@ import java.util.Collections;
 
 /**
  * Util to resolve Java PIDs and resolve mxBeanServer URL by PID
- * TODO fix it, make it working on to java6, java7 and java8
  */
 public class ProcessListManager {
     private static final String LOCAL_PREFIX = "LOCAL:";
@@ -35,28 +39,27 @@ public class ProcessListManager {
      * @throws IOException
      */
     private Collection<String> checkLPS() throws IOException {
-//		ProcessBuilder pb = new ProcessBuilder("jps");
-//		Process process = pb.start();
-//		BufferedReader br = null;
-//		try {
-//			br = new BufferedReader(new InputStreamReader(process.getInputStream()));
-//			ArrayList<String> list = new ArrayList<String>();
-//			String l ;
-//			while((l = br.readLine()) != null) {
-//				String[] ll = l.split(" ", 2);
-//				if (ll[1].equalsIgnoreCase("jps")) continue;
-//				int pid = Integer.valueOf(ll[0]);
-//				if (ConnectorAddressLink.importFrom(pid) != null) {
-//					list.add(LOCAL_PREFIX + pid + " - " + ll[1]);
-//				}
-//			}
-//			return list;
-//		} finally {
-//			if (br != null) {
-//				br.close();
-//			}
-//		}
-        return Collections.emptyList(); // TODO
+		ProcessBuilder pb = new ProcessBuilder("jps");
+		Process process = pb.start();
+		BufferedReader br = null;
+		try {
+			br = new BufferedReader(new InputStreamReader(process.getInputStream()));
+			ArrayList<String> list = new ArrayList<String>();
+			String l ;
+			while((l = br.readLine()) != null) {
+				String[] ll = l.split(" ", 2);
+				if (ll[1].equalsIgnoreCase("jps")) continue;
+				int pid = Integer.valueOf(ll[0]);
+				if (ConnectorAddressLink.importFrom(pid) != null) {
+					list.add(LOCAL_PREFIX + pid + " - " + ll[1]);
+				}
+			}
+			return list;
+		} finally {
+			if (br != null) {
+				br.close();
+			}
+		}
     }
 
 
@@ -85,9 +88,9 @@ public class ProcessListManager {
             s = s.substring(0, index - 1).trim();
         }
         int pid = Integer.parseInt(s);
-        String serviceURL = null; // TODO //ConnectorAddressLink.importFrom(pid);
+        String serviceURL = ConnectorAddressLink.importFrom(pid);
         if (serviceURL == null) {
-            throw new IOException("Cannot connect to local process - " + pid);
+            throw new IOException("Cannot connect to local process - " + pid + "(");
         }
         return new JMXServiceURL(serviceURL);
     }
