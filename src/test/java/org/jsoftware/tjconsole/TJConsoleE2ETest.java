174d4ee376557023dde3656014d597c460603080
m-szalik <szalik@jsoftware.org>
2016-03-07 16:39:15 +0100

    Tests

--- src/test/java/org/jsoftware/tjconsole/TJConsoleE2ETest.java
+++ src/test/java/org/jsoftware/tjconsole/TJConsoleE2ETest.java
@@ -3,7 +3,6 @@ package org.jsoftware.tjconsole;
 import org.jsoftware.tjconsole.console.Output;
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import javax.management.MBeanServer;
@@ -11,6 +10,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.lang.management.ManagementFactory;
 import java.util.Properties;
+import java.util.regex.Pattern;
 
 public class TJConsoleE2ETest {
     private TestOutputHelper outputHelper;
@@ -50,12 +50,12 @@ public class TJConsoleE2ETest {
         Assert.assertTrue(outputHelper.toString().length() > 0);
     }
 
-    @Test @Ignore
+    @Test
     public void testCmdGet() throws Exception {
-        tjConsole.executeCommand("use java.lang:type=Memory", outputHelper.getOutput());
+        tjConsole.executeCommand("use java.lang:type=Runtime", outputHelper.getOutput());
         outputHelper.clear();
-        tjConsole.executeCommand("get ObjectName", outputHelper.getOutput());
-        Assert.assertEquals("ObjectName=java.lang:type=Memory", outputHelper.toString().trim().replace(" ", ""));
+        tjConsole.executeCommand("get StartTime", outputHelper.getOutput());
+        outputHelper.matchesPattern("StartTime = \\d*");
     }
 
     @Test
@@ -64,7 +64,7 @@ public class TJConsoleE2ETest {
         tjConsole.executeCommand("set Verbose=true", outputHelper.getOutput());
         outputHelper.clear();
         tjConsole.executeCommand("get Verbose", outputHelper.getOutput());
-        Assert.assertTrue(outputHelper.toString().contains("Verbose = true"));
+        Assert.assertEquals("Verbose = true", outputHelper.toString().trim());
     }
 
     @Test
@@ -75,6 +75,35 @@ public class TJConsoleE2ETest {
         Assert.assertEquals("Methodresult:(void)", outputHelper.toString().trim().replace(" ", ""));
     }
 
+    @Test
+    public void testCmdEnv() throws Exception {
+        tjConsole.executeCommand("env", outputHelper.getOutput());
+        Assert.assertTrue(outputHelper.toString().contains("DATE_FORMAT"));
+        tjConsole.executeCommand("env DATE_FORMAT=yyyy-MMM-dd", outputHelper.getOutput());
+        outputHelper.clear();
+        tjConsole.executeCommand("env", outputHelper.getOutput());
+        System.out.println(outputHelper);
+        Assert.assertTrue(outputHelper.toString().contains("DATE_FORMAT = yyyy-MMM-dd"));
+    }
+
+    @Test
+    public void testCmdPs() throws Exception {
+        Pattern processLinePattern = Pattern.compile("^\\d{1,} .*$");
+        tjConsole.executeCommand("ps", outputHelper.getOutput());
+        String[] processList = outputHelper.toString().trim().split("\n");
+        int psCount = 0;
+        for(String psLine : processList) {
+            psLine = psLine.trim();
+            if (psLine.length() == 0) {
+                continue;
+            }
+            psCount++;
+            if (! processLinePattern.matcher(psLine).matches()) {
+                Assert.fail("Line '" + psLine + "' doesn't look like ps line!");
+            }
+        }
+        Assert.assertTrue("We don't have even one process on the list", psCount > 0);
+    }
 }
 
 class TestOutputHelper {
@@ -90,6 +119,14 @@ class TestOutputHelper {
 
     public void clear() {
         out.reset();
+    }
+
+    public void matchesPattern(final String patternStr) {
+        Pattern pattern = Pattern.compile(patternStr);
+        String str = toString().trim();
+        if (! pattern.matcher(str).matches()) {
+            Assert.fail("Input '" + str + "' doesn't match regexp '" + patternStr + "'");
+        }
     }
 }
 
